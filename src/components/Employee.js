45d8f98bf9a3d816c9f76f9909ff337ea35064b7
verangade <verangades@gmail.com>
2021-11-12 00:23:21 +0800

    add checkbox and onChange

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,13 +1,15 @@
 import React from "react"
 
 function Employee(props){
+
+    const val = false;
   
         return(
        /* <li className="Employee">
             {this.props.employeeData.firstName}  {this.props.employeeData.lastName} 
         </li> */
         <li className="Employee">
-            {props.empData.firstName}  {props.empData.lastName} 
+           <input type="checkbox" onChange={ () => console.log("checked")} checked={val}/> {props.empData.firstName}  {props.empData.lastName} 
         </li>
 
         ); 
