73f2ae639157260a456e9b7584f08197956060e9
verangade <verangades@gmail.com>
2021-11-12 00:06:29 +0800

    convert class component to function component.

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,17 +1,17 @@
-import React, {Component} from "react"
+import React from "react"
 
-class Employee extends Component{
-    render(){
+function Employee(props){
+  
         return(
        /* <li className="Employee">
             {this.props.employeeData.firstName}  {this.props.employeeData.lastName} 
         </li> */
         <li className="Employee">
-            {this.props.empData.firstName}  {this.props.empData.lastName} 
+            {props.empData.firstName}  {props.empData.lastName} 
         </li>
 
         ); 
-    }
+    
 }
 
 export default Employee
