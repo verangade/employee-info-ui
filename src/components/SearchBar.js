3b0480af9d1f17e21e4986be3dcc6c9cd9ef5cef
verangade <verangades@gmail.com>
2021-11-17 23:01:24 +0800

    add text box and onChange method

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,10 +3,29 @@ import React , {Component} from "react"
 
 class SearchBar extends Component{
 
+    state = {
+        employee : ""
+    };
+
+    onChange = e => {
+        console.log(e.target.value)
+
+        this.setState({
+            employee : e.target.value
+        });
+    }
+
     render(){
 
         const REACT_VERSION = React.version;
-        return(<div className="SearchBar">Search {REACT_VERSION}</div>);
+        return(<div className="SearchBar">Search {REACT_VERSION}
+            <form>
+                <input type="text" placeholder="Search Employee" value={this.state.employee} onChange={this.onChange}/>
+                <button>Search</button>
+            </form>
+        
+        
+        </div>);
     }
 }
 
